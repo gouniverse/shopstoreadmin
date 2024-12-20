7471479045bf65773878bc6cdc7d3da8c4c4f5d8
Sinevia <info@sinevia.com>
2024-12-20 02:29:29 +0000

    added product media tab

--- go.mod
+++ go.mod
@@ -5,10 +5,10 @@ go 1.23.3
 require (
 	github.com/gouniverse/bs v0.13.0
 	github.com/gouniverse/cdn v1.5.0
-	github.com/gouniverse/form v0.15.4
+	github.com/gouniverse/form v0.16.0
 	github.com/gouniverse/hb v1.80.1
 	github.com/gouniverse/sb v0.8.0
-	github.com/gouniverse/shopstore v0.12.1
+	github.com/gouniverse/shopstore v0.13.0
 	github.com/gouniverse/utils v1.45.4
 	github.com/samber/lo v1.47.0
 	github.com/spf13/cast v1.7.1
@@ -22,7 +22,7 @@ require (
 	github.com/georgysavva/scany v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gouniverse/api v1.6.0 // indirect
-	github.com/gouniverse/base v0.4.0
+	github.com/gouniverse/base v0.5.0
 	github.com/gouniverse/crypto v0.2.0 // indirect
 	github.com/gouniverse/dataobject v0.3.0 // indirect
 	github.com/gouniverse/envenc v0.8.0 // indirect
--- product_update.go
+++ product_update.go
@@ -22,6 +22,7 @@ import (
 
 const viewContent = "content"
 const viewMetadata = "metadata"
+const viewMedia = "media"
 const viewSettings = "settings"
 
 // ===========================================================================
@@ -59,9 +60,14 @@ func (c *productUpdateController) ToTag() hb.TagInterface {
 
 	c.opts.GetLayout().SetTitle("Edit Product | Shop")
 	c.opts.GetLayout().SetBody(c.page(data).ToHTML())
+	c.opts.GetLayout().SetStyleURLs([]string{
+		cdn.TrumbowygCss_2_27_3(),
+	})
 	c.opts.GetLayout().SetScriptURLs([]string{
 		cdn.Htmx_2_0_0(),
 		cdn.Sweetalert2_10(),
+		cdn.Jquery_3_7_1(),
+		cdn.TrumbowygJs_2_27_3(),
 	})
 
 	return hb.Raw(c.opts.GetLayout().Render(c.opts.GetResponseWriter(), c.opts.GetRequest()))
@@ -77,17 +83,10 @@ func (controller *productUpdateController) ToHTML() string {
 
 func (c *productUpdateController) page(data productUpdateControllerData) hb.TagInterface {
 	productManegerURL := url(c.opts.GetRequest(), pathProducts, map[string]string{})
+
 	productUpdateURL := url(c.opts.GetRequest(), pathProductUpdate, map[string]string{
 		"productID": data.productID,
 	})
-	// productUpdateSaveDetailsURL := url(c.opts.GetRequest(), pathProductUpdate, map[string]string{
-	// 	"productID": data.productID,
-	// 	"action":    "update-details",
-	// })
-	// productUpdateSaveMetadataURL := url(c.opts.GetRequest(), pathProductUpdate, map[string]string{
-	// 	"productID": data.productID,
-	// 	"action":    "update-metadata",
-	// })
 
 	breadcrumbs := breadcrumbs(c.opts.GetRequest(), []breadcrumb{
 		{
@@ -100,59 +99,11 @@ func (c *productUpdateController) page(data productUpdateControllerData) hb.TagI
 		},
 	})
 
-	// buttonDetailsSave := hb.Button().
-	// 	Class("btn btn-primary ms-2 float-end").
-	// 	Child(hb.I().Class("bi bi-save").Style("margin-top:-4px;margin-right:8px;font-size:16px;")).
-	// 	HTML("Save").
-	// 	HxInclude("#FormProductUpdate").
-	// 	HxPost(productUpdateSaveDetailsURL).
-	// 	HxTarget("#FormProductUpdate")
-
 	buttonCancel := hb.Hyperlink().
 		Class("btn btn-secondary ms-2 float-end").
 		Child(hb.I().Class("bi bi-chevron-left").Style("margin-top:-4px;margin-right:8px;font-size:16px;")).
 		HTML("Back").
 		Href(productManegerURL)
-
-	// buttonMetadataSave := hb.Button().
-	// 	Class("btn btn-primary ms-2 float-end").
-	// 	Child(hb.I().Class("bi bi-save").Style("margin-top:-4px;margin-right:8px;font-size:16px;")).
-	// 	HTML("Save").
-	// 	HxInclude("#FormProductMetadataUpdate").
-	// 	HxPost(productUpdateSaveMetadataURL).
-	// 	HxTarget("#FormProductMetadataUpdate")
-
-	// cardProductDetails := hb.Div().
-	// 	Class("card").
-	// 	Child(
-	// 		hb.Div().
-	// 			Class("card-header").
-	// 			Style(`display:flex;justify-content:space-between;align-items:center;`).
-	// 			Child(hb.Heading4().
-	// 				HTML("Product Details").
-	// 				Style("margin-bottom:0;display:inline-block;")).
-	// 			Child(buttonDetailsSave),
-	// 	).
-	// 	Child(
-	// 		hb.Div().
-	// 			Class("card-body").
-	// 			Child(c.formDetails(data)))
-
-	// cardProductMetadata := hb.Div().
-	// 	Class("card").
-	// 	Child(
-	// 		hb.Div().
-	// 			Class("card-header").
-	// 			Style(`display:flex;justify-content:space-between;align-items:center;`).
-	// 			Child(hb.Heading4().
-	// 				HTML("Product Metadata").
-	// 				Style("margin-bottom:0;display:inline-block;")).
-	// 			Child(buttonMetadataSave),
-	// 	).
-	// 	Child(
-	// 		hb.Div().
-	// 			Class("card-body").
-	// 			Child(c.formMetadata(data)))
 
 	buttonSave := hb.Button().
 		Class("btn btn-primary ms-2 float-end").
@@ -209,6 +160,11 @@ func (controller *productUpdateController) tabs(data productUpdateControllerData
 		"view":       viewContent,
 	})
 
+	viewMedia := url(controller.opts.GetRequest(), pathProductUpdate, map[string]string{
+		"product_id": data.productID,
+		"view":       viewMedia,
+	})
+
 	viewMetadataURL := url(controller.opts.GetRequest(), pathProductUpdate, map[string]string{
 		"product_id": data.productID,
 		"view":       viewMetadata,
@@ -226,6 +182,11 @@ func (controller *productUpdateController) tabs(data productUpdateControllerData
 				ClassIf(data.view == viewContent, "active").
 				Href(viewContentURL).
 				HTML("Content"))).
+		Child(bs.NavItem().
+			Child(bs.NavLink().
+				ClassIf(data.view == viewMedia, "active").
+				Href(viewMedia).
+				HTML("Media"))).
 		Child(bs.NavItem().
 			Child(bs.NavLink().
 				ClassIf(data.view == viewMetadata, "active").
@@ -251,6 +212,10 @@ func (c *productUpdateController) form(data productUpdateControllerData) hb.TagI
 
 	if data.view == viewMetadata {
 		formProductUpdate.SetFields(c.formMetadataFields(data))
+	}
+
+	if data.view == viewMedia {
+		formProductUpdate.SetFields(c.formMediaFields(data))
 	}
 
 	if data.view == viewSettings {
@@ -308,14 +273,93 @@ func (c *productUpdateController) formContentFields(data productUpdateController
 	description := form.NewField(form.FieldOptions{
 		Label: "Description",
 		Name:  "product_description",
-		Type:  form.FORM_FIELD_TYPE_TEXTAREA,
+		Type:  form.FORM_FIELD_TYPE_HTMLAREA,
 		Value: data.formDescription,
 		Help:  `The description of the product.`,
 	})
 
+	shortDescription := form.NewField(form.FieldOptions{
+		Label: "Short Description",
+		Name:  "product_short_description",
+		Type:  form.FORM_FIELD_TYPE_HTMLAREA,
+		Value: data.formShortDescription,
+		Help:  `The short description of the product.`,
+	})
+
 	return []form.FieldInterface{
 		title,
+		shortDescription,
 		description,
+	}
+}
+
+func (c *productUpdateController) formMediaFields(data productUpdateControllerData) []form.FieldInterface {
+	repeaterAddURL := url(c.opts.GetRequest(), pathProductUpdate, map[string]string{
+		"product_id": data.productID,
+		"view":       viewMedia,
+		"action":     "add",
+	})
+
+	repeaterMoveUpURL := url(c.opts.GetRequest(), pathProductUpdate, map[string]string{
+		"product_id": data.productID,
+		"view":       viewMedia,
+		"action":     "move_up",
+	})
+
+	repeaterMoveDownURL := url(c.opts.GetRequest(), pathProductUpdate, map[string]string{
+		"product_id": data.productID,
+		"view":       viewMedia,
+		"action":     "move_down",
+	})
+
+	repeaterRemoveURL := url(c.opts.GetRequest(), pathProductUpdate, map[string]string{
+		"product_id": data.productID,
+		"view":       viewMedia,
+		"action":     "remove",
+	})
+
+	fieldID := form.NewField(form.FieldOptions{
+		ID:    "product_media_id",
+		Label: "ID",
+		Name:  "product_media[id]",
+		Type:  form.FORM_FIELD_TYPE_STRING,
+		Help:  `The ID of the media.`,
+	})
+
+	fieldTitle := form.NewField(form.FieldOptions{
+		ID:    "product_media_title",
+		Label: "Title",
+		Name:  "product_media[title]",
+		Type:  form.FORM_FIELD_TYPE_STRING,
+		Help:  `The Title of the media.`,
+	})
+
+	fieldURL := form.NewField(form.FieldOptions{
+		ID:    "product_media_url",
+		Label: "URL",
+		Name:  "product_media[url]",
+		Type:  form.FORM_FIELD_TYPE_STRING,
+		Help:  `The URL of the media.`,
+	})
+
+	repeater := form.NewRepeater(form.RepeaterOptions{
+		Label:               "Media",
+		Help:                `The media of the product.`,
+		Name:                "product_media",
+		Values:              data.formMedia,
+		RepeaterAddUrl:      repeaterAddURL,
+		RepeaterMoveUpUrl:   repeaterMoveUpURL,
+		RepeaterMoveDownUrl: repeaterMoveDownURL,
+		RepeaterRemoveUrl:   repeaterRemoveURL,
+		Fields: []form.FieldInterface{
+			fieldID,
+			fieldTitle,
+			fieldURL,
+		},
+	})
+
+	return []form.FieldInterface{
+		repeater,
 	}
 }
 
@@ -631,6 +675,33 @@ func (c *productUpdateController) saveProductSettings(data productUpdateControll
 // 	return reqArrayOfMaps
 // }
 
+func (c *productUpdateController) saveProductMedia(data productUpdateControllerData) (d productUpdateControllerData, errorMessage string) {
+	media := req.Maps(c.opts.GetRequest(), "product_media", []map[string]string{})
+
+	cfmt.Infoln(media)
+
+	productMedia := lo.Map(media, func(m map[string]string, index int) map[string]string {
+		id := strings.TrimSpace(m["id"])
+		url := strings.TrimSpace(m["url"])
+		entry := map[string]string{}
+		entry["id"] = id
+		entry["url"] = url
+		return entry
+	})
+
+	data.formMedia = productMedia
+
+	cfmt.Successln(data.formMedia)
+
+	if data.action == "add" {
+		data.formMedia = append(data.formMedia, map[string]string{
+			"url": "",
+		})
+		return data, ""
+	}
+	return data, ""
+}
+
 func (c *productUpdateController) saveProductMetadata(data productUpdateControllerData) (d productUpdateControllerData, errorMessage string) {
 	metas := req.Maps(c.opts.GetRequest(), "product_meta", []map[string]string{})
 
@@ -673,6 +744,7 @@ func (c *productUpdateController) saveProductMetadata(data productUpdateControll
 
 func (c *productUpdateController) saveProductContent(data productUpdateControllerData) (d productUpdateControllerData, errorMessage string) {
 	data.formDescription = utils.Req(c.opts.GetRequest(), "product_description", "")
+	data.formShortDescription = utils.Req(c.opts.GetRequest(), "product_short_description", "")
 	data.formTitle = utils.Req(c.opts.GetRequest(), "product_title", "")
 
 	if data.formTitle == "" {
@@ -681,6 +753,7 @@ func (c *productUpdateController) saveProductContent(data productUpdateControlle
 	}
 
 	data.product.SetDescription(data.formDescription)
+	data.product.SetShortDescription(data.formShortDescription)
 	data.product.SetTitle(data.formTitle)
 
 	err := c.opts.GetStore().ProductUpdate(context.Background(), data.product)
@@ -730,6 +803,16 @@ func (c *productUpdateController) prepareDataAndValidate() (data productUpdateCo
 		return data, "Product metas not found"
 	}
 
+	media, err := c.opts.GetStore().MediaList(context.Background(), shopstore.NewMediaQuery().
+		SetEntityID(product.ID()))
+
+	if err != nil {
+		c.opts.GetLogger().Error("At shopstoreadmin > productUpdateController > prepareDataAndValidate", "error", err.Error())
+		return data, "Product media not found"
+	}
+
+	data.media = media
+
 	data.formMemo = data.product.Memo()
 	data.formStatus = data.product.Status()
 	data.formTitle = data.product.Title()
@@ -737,6 +820,13 @@ func (c *productUpdateController) prepareDataAndValidate() (data productUpdateCo
 	data.formPrice = data.product.Price()
 	data.formQuantity = data.product.Quantity()
 	data.formMetas = metas
+	data.formMedia = lo.Map(data.media, func(media shopstore.MediaInterface, index int) map[string]string {
+		return map[string]string{
+			"id":   media.ID(),
+			"name": media.Title(),
+			"url":  media.URL(),
+		}
+	})
 
 	if c.opts.GetRequest().Method != http.MethodPost {
 		return data, ""
@@ -744,6 +834,10 @@ func (c *productUpdateController) prepareDataAndValidate() (data productUpdateCo
 
 	if data.view == viewContent {
 		return c.saveProductContent(data)
+	}
+
+	if data.view == viewMedia {
+		return c.saveProductMedia(data)
 	}
 
 	if data.view == viewMetadata {
@@ -772,15 +866,18 @@ type productUpdateControllerData struct {
 	action    string
 	productID string
 	product   shopstore.ProductInterface
+	media     []shopstore.MediaInterface
 	view      string
 
-	formErrorMessage   string
-	formSuccessMessage string
-	formDescription    string
-	formMemo           string
-	formMetas          map[string]string
-	formQuantity       string
-	formPrice          string
-	formStatus         string
-	formTitle          string
+	formErrorMessage     string
+	formSuccessMessage   string
+	formDescription      string
+	formMemo             string
+	formMedia            []map[string]string
+	formMetas            map[string]string
+	formQuantity         string
+	formPrice            string
+	formShortDescription string
+	formStatus           string
+	formTitle            string
 }
